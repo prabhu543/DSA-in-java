e162b7111adafe6eff54fc7caf2c9dea671508f1
Prabhu 543 <prabhuprathamesh444@gmail.com>
2025-02-15 13:52:24 +0530

    Addition of 2 nums

---
+++ Q10.java
@@ -0,0 +1,12 @@
+class Solution {
+  static int addition(int a, int b) {
+      return (a+b);
+  }
+}
+public class Main
+{
+public static void main(String[] args) {
+    Solution s = new Solution();
+  System.out.println("Addition of 2 num :" + s.addition(2,2));
+}
+}
